3cf7b7207db21e19a62bfdd27dd4cac3db6c5e0d
KoodiApina <sulonkoodit@gmail.com>
2025-05-11 02:14:05 +0300

    Register addresses for pwm and gpio channels A and B

--- include/init.h
+++ include/init.h
@@ -2,8 +2,21 @@
 #define INIT_H
 
 #define RCC_APB2ENR (*(volatile unsigned int*)0x40021018)
+
+#define GPIOA_CRH (*(volatile unsigned int*)0x40010804)
+#define GPIOB_CRH (*(volatile unsigned int*)0x40010C04)
 #define GPIOC_CRH (*(volatile unsigned int*)0x40011004)
 
-void gpio_init();
+#define TIM1_PSC (*(volatile unsigned int*)0x40012C28)
+#define TIM1_ARR (*(volatile unsigned int*)0x40012C2C)
+#define TIM1_CCR1 (*(volatile unsigned int*)0x40012C34)
+#define TIM1_CCMR1 (*(volatile unsigned int*)0x40012C18)
+#define TIM1_CCER (*(volatile unsigned int*)0x40012C20)
+#define TIM1_CR1 (*(volatile unsigned int*)0x40012C00)
+#define TIM1_BDTR (*(volatile unsigned int*)0x40012C44)
+
+
+void blink_init();
+void motor_init();
 
 #endif
--- src/init.c
+++ src/init.c
@@ -1,9 +1,37 @@
 #include "../include/init.h"
 
-void gpio_init() 
+// Blink init
+void blink_init() 
 {
     RCC_APB2ENR |= (1 << 4);
 
     GPIOC_CRH &= ~(0xF << 20);
     GPIOC_CRH |= (0x2 << 20);
+}
+
+void timer_init() 
+{
+    // Timer clock = 1mhz, 20ms period as 50hz and 1.5ms pulse
+    TIM1_PSC = 72 -1;
+    TIM1_ARR = 20000 - 1;
+    TIM1_CCR1 = 1500;
+
+    TIM1_CCMR1 |= (0x6E << 4); // Enable PWM mode 1
+    TIM1_CCMR1 |= (1 << 3);    // Preload register on TIMx_CCR1 enabled
+    TIM1_CCER |= (1 << 0);     // CC1E enable
+    TIM1_CR1 |= (1 << 7);      // Enable auto preload
+    TIM1_CR1 |= (1 << 0);      // Enable counter
+    TIM1_BDTR |= (1 << 15);    // Enable Main output
+}
+
+// Init motor pins for X,Y,Z axis
+void motor_init() 
+{
+    RCC_APB2ENR |= (0xB << 2); // Enable gpio channels A and B
+    RCC_APB2ENR |= (1 << 11);  // Enable Timer 1 clock
+
+    // Init timer bits and calculate clock speed
+    timer_init();
+    
+    GPIOA_CRH |= (0x3F3 << 0) | (0x3F3 << 4) | (0x3F3 << 8);
 }
