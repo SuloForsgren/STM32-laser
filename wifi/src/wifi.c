8f32b6126a6425fc39af01c0829ba08187a7acc2
KoodiApina <sulonkoodit@gmail.com>
2025-05-20 00:17:35 +0300

    Wifi working!

--- wifi/src/wifi.c
+++ wifi/src/wifi.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
-#if 0
 #include "pico/stdlib.h"
-#endif
+#include "pico/cyw43_arch.h"
 
+#define SSID "DNA-Mokkula-Eteinen"
+#define PASS "17399557522"
 
-int main(void) 
+void init_wifi()
 {
-    printf("Hello world!\n");
+    cyw43_arch_init_with_country(CYW43_COUNTRY_FINLAND);
+    cyw43_arch_poll();
+}
+
+void connect_wifi()
+{
+    cyw43_arch_enable_sta_mode();
+    cyw43_arch_wifi_connect_blocking(SSID, PASS, CYW43_AUTH_WPA2_AES_PSK);
+}
+
+int main() {
+    stdio_init_all();
+    printf("Booting..\n");
+
+    init_wifi();
+    connect_wifi();
+
     return 0;
 }
